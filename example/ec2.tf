fd618a485b87919efcaabbd677adbaed8e09579f
Lakshmi <narayan.training1@gmail.com>
2023-07-20 10:59:21 +0530

    auto push

--- example/ec2.tf
+++ example/ec2.tf
@@ -1,8 +1,9 @@
-resource "aws_instance" "web" {
-  ami           = "ami-0c1d144c8fdd8d690"
-  instance_type = "t3.micro"
+resource "aws_instance" "server" {
+  ami                     = "ami-0c1d144c8fdd8d690"
+  instance_type           = "t3.nano"
+  vpc_security_group_ids  = [aws_security_group.allow_all.id]
 
   tags = {
-    Name = "HelloWorld"
+    Name = "TerraformServer"
   }
 }
