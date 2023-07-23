1649902fc312337ea92dfe20f578d718b1987e4f
Lakshmi <narayan.training1@gmail.com>
2023-07-23 16:10:28 +0530

    auto push

---
+++ 07-modules/sg/main.tf
@@ -0,0 +1,24 @@
+resource "aws_security_group" "allows_tls" {
+  name        = "allows_tls"
+  description = "Allows TLS inbound traffic"
+ 
+
+  ingress {
+    description = "TLS from VPC"
+    from_port   = 443
+    to_port     = 443
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  tags = {
+    Name = "allows_tls"
+  }
+}
