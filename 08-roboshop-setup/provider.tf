90b14ab15c8c2fdfa84db39a3b60e375acf1c449
Lakshmi <narayan.training1@gmail.com>
2023-07-30 07:15:24 +0530

    auto push

--- 08-roboshop-setup/provider.tf
+++ 08-roboshop-setup/provider.tf
@@ -1,6 +1,6 @@
 terraform {
   backend "s3" {
-    bucket         = "b54-tf-remote-state"
+    bucket         = "b54-tf-remotesite"
     key            = "robot/terraform.tfstate"     # This is like the folder path that you prefer giving
     region         = "us-east-1"
   }
