cb48d8f702a41a5e2b4391358d4e3180e2e58992
Lakshmi <narayan.training1@gmail.com>
2023-07-30 06:54:32 +0530

    auto push

--- 08-roboshop-setup/ec2/data.tf
+++ 08-roboshop-setup/ec2/data.tf
@@ -1,5 +1,5 @@
 data "aws_ami" "image" {
   most_recent      = true
-  name_regex       = "ansible-lab-image"     # Ensure you make your own AMI from my labImage and install ANSIBLE on the top of it and then use your AMI Name
+  name_regex       = "ansible-lab-image1"     # Ensure you make your own AMI from my labImage and install ANSIBLE on the top of it and then use your AMI Name) ansible.noarch 0:2.9.27-1.el7  )
   owners           = ["self"]
 }
